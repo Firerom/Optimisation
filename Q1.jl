79b69bd30b036bb7cf7bd72b0089464f95422fe3
jor15 <joris_hard@yahoo.fr>
2017-12-05 10:16:03 +0100

    Rajout small_epsilon dans la première contrainte

--- Q1.jl
+++ Q1.jl
@@ -1,5 +1,5 @@
 include("optimisation1.jl")
-fichier="obs_behind"
+fichier="obs_behind_side"
 Data1=loadDataFromFile(fichier)
 small_epsilon=0.000001
 #start: (50,100,0) [x,y,theta]
@@ -59,7 +59,7 @@ theta_i=Data1.start[3];
 #@constraint(m, -(c7+c8+c9)>=0.001)
 
 #First constraint initially in S_safe
-@constraint(m, c0+c1*x_i+c2*y_i+c3*theta_i+c4*x_i*y_i+c5*x_i*theta_i+c6*y_i*theta_i+c7*x_i^2+c8*y_i^2+c9*theta_i^2<=b)
+@constraint(m, c0+c1*x_i+c2*y_i+c3*theta_i+c4*x_i*y_i+c5*x_i*theta_i+c6*y_i*theta_i+c7*x_i^2+c8*y_i^2+c9*theta_i^2+small_epsilon<=b)
 
 #Second constraint with the obstacles (M_obs already SDP), for each of them
 for i=1:Nbr_obstacle
