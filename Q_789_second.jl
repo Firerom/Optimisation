93500af9b7d5aa72e1060c89e5a3a47be7f39b36
firerom <romain63@hotmail.com>
2017-12-08 09:15:27 +0100

    modif

--- Q_789_second.jl
+++ Q_789_second.jl
@@ -1,14 +1,14 @@
 include("optimisation1.jl")
-fichier="obs_behind"
+fichier="romain"
 Data1=loadDataFromFile(fichier)
-
+#Data1=generateData(4)
 #obstacles = Array{Tuple{Float64,Float64}}(4)
 #obstacles[1] = (40, 90)
 #obstacles[2] = (60, 90)
 #obstacles[3] = (47, 110)
 #obstacles[4] = (50, 110)
 #Data1 = Optinum.Data((50.0, 100.0, 0.0), (50.0, 190.0), obstacles)
-
+tolerance=0.05
 small_epsilon=0.000001
 #start: (50,100,0) [x,y,theta]
 #destination: (50,190) [x,y]
@@ -23,10 +23,10 @@ u=0 #test for pi after
 v=6
 K=0.2
 b=0
-w=0
+w=-0.25
 Time_of_fligth=20
-Time_step=0.2
-Number_t_step=20
+Time_step=0.1
+Number_t_step=400
 Time_of_fligth=Time_step*Number_t_step
 #L=obstacle_near(fichier,[70 90])
 k=10
@@ -53,7 +53,7 @@ for t=1:1:Number_t_step
 			s_dot=[-v*sin(s_actual[3])+w v*cos(s_actual[3]) -K*(s_actual[3]-u[i_u])]
 
 			s_suivant[i_u,:]=s_actual+Time_step*s_dot
-			C=SDP_barrier_test(mes_obstacles,s_actual,u[i_u])
+			C=SDP_barrier(mes_obstacles,s_actual,u[i_u])
 			x=s_suivant[i_u,1]
 			y=s_suivant[i_u,2]
 			theta=s_suivant[i_u,3]
@@ -66,11 +66,21 @@ for t=1:1:Number_t_step
 			c7=C[8]
 			c8=C[9]
 			c9=C[10]
-			grad_V=[(c1+c4*s_actual[2]+c5*s_actual[3]+2*c7*s_actual[1]),(c2+c4*s_actual[1]+c6*s_actual[3]+2*c8*s_actual[2]),(c3+c5*s_actual[1]+c6*s_actual[2]+2*c9*s_actual[3])]
+			#grad_V=[(c1+c4*s_actual[2]+c5*s_actual[3]+2*c7*s_actual[1]),(c2+c4*s_actual[1]+c6*s_actual[3]+2*c8*s_actual[2]),(c3+c5*s_actual[1]+c6*s_actual[2]+2*c9*s_actual[3])]
+			## test pour romain
+			grad_V=[(c1+c4*s_suivant[2]+c5*s_suivant[3]+2*c7*s_suivant[1]),(c2+c4*s_suivant[1]+c6*s_suivant[3]+2*c8*s_suivant[2]),(c3+c5*s_suivant[1]+c6*s_suivant[2]+2*c9*s_suivant[3])]
+			Vec=[1 x y theta x*y x*theta y*theta x^2 y^2 theta^2]
+			prod_scal=C*Vec.'
+			write_console("prod_scal:\n",C*Vec.')
+			##
 			write_console(C,"")
 			write_console("V_dot:\n",dot(grad_V,s_dot))
-			if(dot(grad_V,s_dot)<=0)
+			#if(dot(grad_V,s_dot)<=tolerance )
+			## test pour romain
+			if(dot(grad_V,s_dot)<=tolerance )
+				if(prod_scal[1]<tolerance)
 				u_possible=[u_possible i_u]
+				end
 			end
 		end
 
@@ -129,3 +139,7 @@ for t=1:1:Number_t_step
 	u_possible=[0]
 
 end
+
+write_console("S_trajet: ",s_enreg)
+write_console("Obsta: ",Data1.obstacles)
+write_console("Destin: ",s_final)
