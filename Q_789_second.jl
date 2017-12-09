748b85a6992c1d1f13ce9b7aa8538efbd0e29190
firerom <romain63@hotmail.com>
2017-12-09 15:58:00 +0100

    cou

--- Q_789_second.jl
+++ Q_789_second.jl
@@ -1,15 +1,17 @@
 include("optimisation1.jl")
 
-fichier="Romain2"
+fichier="Romain"
 
 # if load from a file
-#Data1=loadDataFromFile(fichier)
-#saveDataToFileTXT(Data1,fichier)
-# if generateData of n obstacle
-Data1=generateData(25)
+Data1=loadDataFromFile(fichier)
 saveDataToFileTXT(Data1,fichier)
-saveDataToFile(Data1,fichier)
-tolerance=0.05
+
+# if generateData of n obstacle
+#Data1=generateData(30)
+#saveDataToFileTXT(Data1,fichier)
+#saveDataToFile(Data1,fichier)
+
+tolerance=0.00
 small_epsilon=0.000001
 
 
@@ -23,18 +25,32 @@ b=0
 w=0
 Time_of_fligth=20
 Time_step=0.1
-Number_t_step=400
+Number_t_step=150
 Time_of_fligth=Time_step*Number_t_step
 #L=obstacle_near(fichier,[70 90])
-k=20
+k=21
+nbr_iteration_SDP=20
+nbr_iteration_free=2
+
 u=-pi:(2*pi/(k-1)):pi
 Current_time=0
+
+
 u_possible=zeros(k)
 s_suivant=zeros(k,3)
+
+s_suivantSDP=zeros(3)
+s_suivantfree=zeros(3)
+
 s_final=[Data1.destination[1] Data1.destination[2]]
 s_connu=[Data1.start[1] Data1.start[2] Data1.start[3]]
 s_enreg=zeros(Number_t_step,3)
-for t=1:1:Number_t_step
+t=1
+
+#distSDP=zeros(nbr_iteration_SDP,1)
+#distfree=zeros(nbr_iteration_free,1)
+while t<=Number_t_step
+	write_console("t: ",t)
 	u_possible=0
 	Current_time+=Time_step
 	s_actual=s_connu
@@ -42,58 +58,44 @@ for t=1:1:Number_t_step
 
 	mes_obstacles=mes_obstacles_near[1]
 	number_obst_near=mes_obstacles_near[2]
-
+	write_console("number_obst_near: ",number_obst_near)
+	write_console("mes_obstacles: ",mes_obstacles)
 	if(number_obst_near!=0)# il y a des obstacles
 	#phase avec le calcul de SDP pour V  car obstacle
-		for i_u=1:1:k
-			i_u=round(i_u)
-			s_dot=[-v*sin(s_actual[3])+w v*cos(s_actual[3]) -K*(s_actual[3]-u[i_u])]
 
-			s_suivant[i_u,:]=s_actual+Time_step*s_dot
-			C=SDP_barrier(mes_obstacles,s_actual,u[i_u])
-			x=s_suivant[i_u,1]
-			y=s_suivant[i_u,2]
-			theta=s_suivant[i_u,3]
-			c1=C[2]
-			c2=C[3]
-			c3=C[4]
-			c4=C[5]
-			c5=C[6]
-			c6=C[7]
-			c7=C[8]
-			c8=C[9]
-			c9=C[10]
-			#grad_V=[(c1+c4*s_actual[2]+c5*s_actual[3]+2*c7*s_actual[1]),(c2+c4*s_actual[1]+c6*s_actual[3]+2*c8*s_actual[2]),(c3+c5*s_actual[1]+c6*s_actual[2]+2*c9*s_actual[3])]
-			## test pour romain
-			grad_V=[(c1+c4*s_suivant[2]+c5*s_suivant[3]+2*c7*s_suivant[1]),(c2+c4*s_suivant[1]+c6*s_suivant[3]+2*c8*s_suivant[2]),(c3+c5*s_suivant[1]+c6*s_suivant[2]+2*c9*s_suivant[3])]
-			Vec=[1 x y theta x*y x*theta y*theta x^2 y^2 theta^2]
-			prod_scal=C*Vec.'
-			write_console("prod_scal:\n",C*Vec.')
-			##
-			write_console(C,"")
-			write_console("V_dot:\n",dot(grad_V,s_dot))
-			#if(dot(grad_V,s_dot)<=tolerance )
-			## test pour romain
-			if(dot(grad_V,s_dot)<=tolerance )
-				if(prod_scal[1]<tolerance)
+		s_garderSDP=zeros(k,nbr_iteration_SDP,3)
+		for i_u=1:k
+			i_u=round(i_u)
+
+			s_temp=s_actual
+			for i=1:nbr_iteration_SDP
+				s_dot=[-v*sin(s_temp[3])+w v*cos(s_temp[3]) -K*(s_temp[3]-u[i_u])]
+				s_suivantSDP=s_temp+Time_step*s_dot
+				s_temp=s_suivantSDP
+				s_garderSDP[i_u,i,:]=s_suivantSDP
+			end
+			s_suivant[i_u,:]=s_suivantSDP
+			C=SDP_barrier(mes_obstacles,s_actual,s_suivantSDP,u[i_u],w,Time_step)
+			write_console("Coeff: \n",C)
+			if(!isnan(C[1]))
 				u_possible=[u_possible i_u]
-				end
 			end
 		end
-
 	else
 		#on peut tout de suite calculer tout les s(notamenet theta)pour tout les u
 		#
+		s_garderfree=zeros(k,nbr_iteration_free,3)
 		for i_u=1:1:k
 			i_u=round(i_u)
-			s_dot=[-v*sin(s_actual[3])+w v*cos(s_actual[3]) -K*(s_actual[3]-u[i_u])]
-			s_suivant[i_u,:]=s_actual+Time_step*s_dot
-			s_possible=s_suivant# tout es possible, car aucune contrainte
-			x=s_suivant[i_u,1]
-			y=s_suivant[i_u,2]
-			theta=s_suivant[i_u,3]
-			u_possible=[u_possible i_u]# on prend tout les indices
-
+			s_temp=s_actual
+			for i=1:nbr_iteration_free
+				s_dot=[-v*sin(s_temp[3])+w v*cos(s_temp[3]) -K*(s_temp[3]-u[i_u])]
+				s_suivantfree=s_temp+Time_step*s_dot
+				s_temp=s_suivantfree
+				s_garderfree[i_u,i,:]=s_suivantfree
+			end
+			s_suivant[i_u,:]=s_suivantfree
+			u_possible=[u_possible i_u]
 		end
 
 	end
@@ -105,36 +107,74 @@ for t=1:1:Number_t_step
 	for i=1:size(s_possible,1)
 		write_console(s_possible[i,:],"")
 	end
-	write_console("\nS suivant(dynamique deja fixee): ",[s_possible[1,1] s_possible[1,2]])
-	write_console("S Final: ",s_final,"\narctg a realiser\n")
+	#write_console("\nS suivant(dynamique deja fixee): ",[s_possible[1,1] s_possible[1,2]])
+	#write_console("S Final: ",s_final,"\narctg a realiser\n")
 
-	alpha=mon_arctg(s_suivant[1,1:2],s_final)
-	alpha=alpha-pi/2
-	write_console("alpha= ",alpha)
-	Aminimiser=s_possible[:,3]-alpha
-	write_console("A minimiser vers 0: ",Aminimiser)
-	optimum=extrema(abs.(Aminimiser))
-	min=optimum[1]
-	Indice_u=find_index(abs.(Aminimiser),min)
+	if(number_obst_near!=0)
+		distSDP=zeros(size(s_possible,1),1)
+		for i=1:size(s_possible,1)
+			distSDP[i]=norm(s_possible[i,1:2].'-s_final)
+
+		end
+		write_console("distSDP= ",distSDP)
+		optimum=extrema(distSDP)
+		min=optimum[1]
+		Indice_u=find_index(distSDP,min)
+		#write_console("Indice_u= ",Indice_u)
+	else
+		distfree=zeros(size(s_possible,1),1)
+		for i=1:size(s_possible,1)
+			distfree[i]=norm(s_possible[i,1:2].'-s_final)
+
+		end
+		write_console("distfree= ",distfree)
+		optimum=extrema(distfree)
+		min=optimum[1]
+		Indice_u=find_index(distfree,min)
+	end
+
 	write_console("Choosen index: ",Indice_u)
 	s_choosen=s_possible[Indice_u,:]
 	write_console("Choosen s: ",s_choosen)
 	u_choosen=u[u_possible[Indice_u+1]]
 	write_console("Choosen u: ",u_choosen)
-	write_console("\n----------------------\n","")
+
 	s_connu=s_choosen.'
-	s_enreg[t,:]=s_connu
+	#s_enreg[t,:]=s_connu
+	if(number_obst_near!=0)
+		write_console("Indice_u: ",Indice_u)
+		write_console("s_garder: ",s_garderSDP[Indice_u,:,:])
+		write_console("s_choosen: ",s_choosen.')
+
+		for i=1:nbr_iteration_SDP
+			s_enreg[t,:]=s_garderSDP[Indice_u+1,i,:]
+			t+=1
+		end
+		t-=1
+		write_console("t: ",t)
+	else
+		write_console("Indice_u: ",Indice_u)
+		write_console("s_garder: ",s_garderfree[Indice_u,:,:])
+			write_console("s_choosen: ",s_choosen.')
+		for i=1:nbr_iteration_free
+			s_enreg[t,:]=s_garderfree[Indice_u+1,i,:]
+			t+=1
+		end
+		t-=1
+		write_console("t: ",t)
+	end
 	#=write_console("s_enreg: ","")
 	for i=1:size(s_enreg,1)
 		write_console(s_enreg[i,:],"")
 	end=#
 	#write_console(norm(s_enreg[t,1:2].'-s_final))
+	write_console("\n----------------------\n","")
 	if(norm(s_enreg[t,1:2].'-s_final)<1)#moins d'un mettre 'lun de lautre ->ok
 		t=Number_t_step
 		break
 	end
 	u_possible=[0]
-
+	t+=1
 end
 
 write_console("S_trajet: ",s_enreg)
--- optimisation1.jl
+++ optimisation1.jl
@@ -129,7 +129,7 @@ end
 function SDP_barrier(obstacle,s_actual,s_suivant,u,w,Time_step)
 
 
-    small_epsilon=0.1
+    small_epsilon=0.001
 
     #le nombre d'obstacle
     Nbr_obstacle=size(obstacle,1)
@@ -142,8 +142,8 @@ function SDP_barrier(obstacle,s_actual,s_suivant,u,w,Time_step)
 
 	#XY_R=Time_step*s_dot
 	#R_max=norm([XY_R[1] XY_R[2]])
-	R=0.5
-	k=6
+	R=2
+	k=4
 	angle=0:(2*pi/(k)):(2*pi)
 	Point=zeros(k,k,2)
 	Point_s=zeros(k,k,2)
@@ -224,11 +224,11 @@ function SDP_barrier(obstacle,s_actual,s_suivant,u,w,Time_step)
 	#X_vec=[1,s_itera[k+1,1],s_itera[k+1,2],s_itera[k+1,3],s_itera[k+1,3]^2,s_itera[k+1,3]^3,s_itera[k+1,3]^4,s_itera[k+1,3]^5,s_itera[k+1,3]^6,s_itera[k+1,3]^7,s_itera[k+1,3]^8,s_itera[k+1,3]^9]
 	@constraint(m,dot(grad_V,s_dot)<=0)
 
-	for i=1:1:k
-		s_dot1=[-v*sin(s_actual[3])+w,v*cos(s_actual[3]),-K*(s_actual[3]-u)]
-		grad_V1=[(c1+c4*Point[i,2]+c5*s_actual[3]+2*c7*Point[i,1]),(c2+c4*Point[i,1]+c6*s_actual[3]+2*c8*Point[i,2]),(c3+c5*Point[i,1]+c6*Point[i,2]+2*c9*s_actual[3])]
-		@constraint(m,dot(grad_V1,s_dot1)<=0)
-	end
+	#for i=1:1:k
+	#	s_dot1=[-v*sin(s_actual[3])+w,v*cos(s_actual[3]),-K*(s_actual[3]-u)]
+	#	grad_V1=[(c1+c4*Point[i,2]+c5*s_actual[3]+2*c7*Point[i,1]),(c2+c4*Point[i,1]+c6*s_actual[3]+2*c8*Point[i,2]),(c3+c5*Point[i,1]+c6*Point[i,2]+2*c9*s_actual[3])]
+	#	@constraint(m,dot(grad_V1,s_dot1)<=0)
+	#end
 
 
     solve(m)
@@ -269,81 +269,7 @@ end
 
 
 
-function SDP_barrier_test(obstacle,s_actual,u)
 
-
-    small_epsilon=0.000001
-
-    #le nombre d'obstacle
-    Nbr_obstacle=size(obstacle,1)
-
-    v=6
-    K=0.2
-    b=0
-
-    m = Model(solver=MosekSolver())
-
-    @variable(m, c0)
-    @variable(m, c1)
-    @variable(m, c2)
-    @variable(m, c3)
-    @variable(m, c4)
-    @variable(m, c5)
-    @variable(m, c6)
-    @variable(m, c7)
-    @variable(m, c8)
-    @variable(m, c9)
-    #matrices SDP for obstacle constraint
-    @variable(m, M_obs[1:Nbr_obstacle,1:2,1:2])
-
-
-    # each matrix obstacle must be symmetric and SDP
-    for n=1:Nbr_obstacle
-    	for i=1:2
-    		for j=i:2
-    			@constraint(m,M_obs[n,i,j]-M_obs[n,j,i]==0)
-    		end
-    	end
-    @SDconstraint(m,M_obs[n,:,:]>=0)
-    end
-
-
-    #V(s) polynome
-    #c0, c1, c2,    c3, c4,     c5,     c6,  c7,  c8,      c9
-    # 1,  x,  y, theta, xy, xtheta, ytheta, x^2, y^2, theta^2
-
-    #Initial configuration of the drone
-    x_i=s_actual[1];
-    y_i=s_actual[2];
-    theta_i=s_actual[3];
-
-
-    #V is minimum degree 2 must be verified or we must impose one more constraint
-    #@constraint(m, -(c7+c8+c9)>=0.001)
-
-    #First constraint initially in S_safe
-    @constraint(m, c0+c1*x_i+c2*y_i+c3*theta_i+c4*x_i*y_i+c5*x_i*theta_i+c6*y_i*theta_i+c7*x_i^2+c8*y_i^2+c9*theta_i^2+small_epsilon<=b)
-
-    #Second constraint with the obstacles (M_obs already SDP), for each of them
-    for i=1:Nbr_obstacle
-		x_o=obstacle[i,1]
-		y_o=obstacle[i,2]
-    	@constraint(m, M_obs[i,1,1]==c0+c1*x_o+c2*y_o+c4*x_o*y_o+c7*x_o^2+c8*y_o^2-b-small_epsilon)
-    	@constraint(m, 2*M_obs[i,1,2]==c3+c5*x_o+c6*y_o)
-    	@constraint(m, M_obs[i,2,2]==c9)
-    end
-
-
-	#s_dot=[-v*sin(s_actual[3])+w,v*cos(s_actual[3]),-K*(s_actual[3]-u)]
-	#grad_V=[(c1+c4*s_actual[2]+c5*s_actual[3]+2*c7*s_actual[1]),(c2+c4*s_actual[1]+c6*s_actual[3]+2*c8*s_actual[2]),(c3+c5*s_actual[1]+c6*s_actual[2]+2*c9*s_actual[3])]
-	#X_vec=[1,s_itera[k+1,1],s_itera[k+1,2],s_itera[k+1,3],s_itera[k+1,3]^2,s_itera[k+1,3]^3,s_itera[k+1,3]^4,s_itera[k+1,3]^5,s_itera[k+1,3]^6,s_itera[k+1,3]^7,s_itera[k+1,3]^8,s_itera[k+1,3]^9]
-	#@constraint(m,dot(grad_V,s_dot)<=0)
-
-    solve(m)
-
-    C=getvalue([c0 c1 c2 c3 c4 c5 c6 c7 c8 c9])
-    return C
-end
 
 function mon_arctg(Initial,Final)#compris entre pi et -pi cercle trigo
 	alpha=atan((-Initial[2]+Final[2])/(Final[1]-Initial[1]))
@@ -357,211 +283,4 @@ function mon_arctg(Initial,Final)#compris entre pi et -pi cercle trigo
 		end
 	end
 	return alpha
-end
-
-function SDP_barrier_jo(obstacle,s_actual,u,w,Time_step)
-	include("optimisation1.jl")
-	fichier="obs_behind_side"
-	Data1=loadDataFromFile(fichier)
-	small_epsilon=0.000001
-	#start: (50,100,0) [x,y,theta]
-	#destination: (50,190) [x,y]
-	#obstacle: x_obstacle1=Data1.obstacles[1][1]
-	#obstacle: y_obstacle1=Data1.obstacles[1][2]
-	#obstacles are behind: (40,90) et (60,90) [90<100]
-
-	#le nombre d'obstacle
-	Nbr_obstacle=size(obstacle,1)
-
-	v=6
-	K=0.2
-	b=0
-
-	#compute a circle around the start(actual) position
-	s_dot=[-v*sin(s_actual[3])+w v*cos(s_actual[3]) -K*(s_actual[3]-u)]
-	s_suivant=s_actual+Time_step*s_dot
-
-	XY_R=Time_step*s_dot
-	R_max=norm([XY_R[1] XY_R[2]])
-	R_max=1.5
-	k=20
-	R=0:R_max/k:R_max
-	angle=0:(2*pi/(k)):(2*pi)
-	Point=zeros(k,k,2)
-	Point_s=zeros(k,k,2)
-	for j=1:1:k
-		for i=1:1:k
-			Point[j,i,1]=R[j]*cos(angle[i])+s_actual[1]
-			Point[j,i,2]=R[j]*sin(angle[i])+s_actual[2]
-			Point_s[j,i,1]=R[j]*cos(angle[i])+s_suivant[1]
-			Point_s[j,i,2]=R[j]*sin(angle[i])+s_suivant[2]
-		end
-	end
-
-
-	m = Model(solver=MosekSolver())
-
-	@variable(m, c0)
-	@variable(m, c1)
-	@variable(m, c2)
-	@variable(m, c3)
-	@variable(m, c4)
-	@variable(m, c5)
-	@variable(m, c6)
-	@variable(m, c7)
-	@variable(m, c8)
-	@variable(m, c9)
-	#matrices SDP for obstacle constraint
-	@variable(m, M_obs[1:Nbr_obstacle,1:2,1:2])
-	#matrix for the Vdot constraint
-	@variable(m,M[1:12,1:12], Symmetric)
-	@SDconstraint(m,M>=0)
-
-	# each matrix obstacle must be symmetric and SDP
-	for n=1:Nbr_obstacle
-		for i=1:2
-			for j=i:2
-				@constraint(m,M_obs[n,i,j]-M_obs[n,j,i]==0)
-			end
-		end
-	@SDconstraint(m,M_obs[n,:,:]>=0)
-	end
-
-
-	#V(s) polynome
-	#c0, c1, c2,    c3, c4,     c5,     c6,  c7,  c8,      c9
-	# 1,  x,  y, theta, xy, xtheta, ytheta, x^2, y^2, theta^2
-
-	#Initial configuration of the drone
-	x_i=s_actual[1];
-	y_i=s_actual[2];
-	theta_i=s_actual[3];
-
-
-	#V is minimum degree 2 must be verified or we must impose one more constraint
-	#@constraint(m, -(c7+c8+c9)>=0.001)
-
-	#First constraint initially in S_safe
-	@constraint(m, c0+c1*x_i+c2*y_i+c3*theta_i+c4*x_i*y_i+c5*x_i*theta_i+c6*y_i*theta_i+c7*x_i^2+c8*y_i^2+c9*theta_i^2+small_epsilon<=b)
-	for j=1:1:k
-		for i=1:1:size(Point,1)
-			x_i=Point[j,i,1]
-			y_i=Point[j,i,2]
-			theta_i=s_actual[3]
-			@constraint(m, c0+c1*x_i+c2*y_i+c3*theta_i+c4*x_i*y_i+c5*x_i*theta_i+c6*y_i*theta_i+c7*x_i^2+c8*y_i^2+c9*theta_i^2+small_epsilon<=b)
-			x_i=Point_s[j,i,1]
-			y_i=Point_s[j,i,2]
-			theta_i=s_suivant[3]
-			@constraint(m, c0+c1*x_i+c2*y_i+c3*theta_i+c4*x_i*y_i+c5*x_i*theta_i+c6*y_i*theta_i+c7*x_i^2+c8*y_i^2+c9*theta_i^2+small_epsilon<=b)
-		end
-	end
-
-	#Second constraint with the obstacles (M_obs already SDP), for each of them
-	for i=1:Nbr_obstacle
-		x_o=obstacle[i,1]
-		y_o=obstacle[i,2]
-		@constraint(m, M_obs[i,1,1]==c0+c1*x_o+c2*y_o+c4*x_o*y_o+c7*x_o^2+c8*y_o^2-b-small_epsilon)
-		@constraint(m, 2*M_obs[i,1,2]==c3+c5*x_o+c6*y_o)
-		@constraint(m, M_obs[i,2,2]==c9)
-	end
-
-	#third constraint V_dot
-
-	# 1
-	@constraint(m,c2*v + K*c3*u==-(M[1,1]))
-	# x
-	@constraint(m,c4*v + K*c5*u==-(2*M[1,2]))
-	# y
-	@constraint(m,2*c8*v + K*c6*u==-(2*M[1,3]))
-	# xt
-	@constraint(m,- K*c5 - 2*c7*v==-(2*M[2,4]))
-	# xt2
-	@constraint(m,-(c4*v)/2==-(2*M[2,5]))
-	# xt3
-	@constraint(m,(2*c7*v)/6==-(2*M[2,6]))
-	# xt4
-	@constraint(m,(c4*v)/24==-(2*M[2,7]))
-	# xt5
-	@constraint(m,-(2*c7*v)/120==-(2*M[2,8]))
-	# xt6
-	@constraint(m,-(c4*v)/720==-(2*M[2,9]))
-	# xt7
-	@constraint(m,(2*c7*v)/5040==-(2*M[2,10]))
-	# xt9
-	@constraint(m,-(2*c7*v)/362880==-(2*M[2,12]))
-	# yt
-	@constraint(m,- K*c6 - c4*v==-(2*M[3,4]))
-	# yt2
-	@constraint(m,-(2*c8*v)/2==-(2*M[3,5]))
-	# yt3
-	@constraint(m,(c4*v)/6==-(2*M[3,6]))
-	# yt4
-	@constraint(m,(2*c8*v)/24==-(2*M[3,7]))
-	# yt5
-	@constraint(m,-(c4*v)/120==-(2*M[3,8]))
-	# yt6
-	@constraint(m,-(2*c8*v)/720==-(2*M[3,9]))
-	# yt7
-	@constraint(m,(c4*v)/5040==-(2*M[3,10]))
-	# yt9
-	@constraint(m,-(c4*v)/362880==-(2*M[3,12]))
-
-
-	# t
-	@constraint(m,c6*v - c1*v - K*c3 + 2*K*c9*u==-(2*M[1,4]))
-	# t2
-	@constraint(m,- 2*K*c9 - c5*v - (c2*v)/2==-(2*M[1,5]+M[4,4]))
-	# t3
-	@constraint(m,(c1*v)/6 - (c6*v)/2==-(2*M[1,6]+M[4,5]+M[4,5]))
-	# t4
-	@constraint(m,(c2*v)/24 + (c5*v)/6==-(2*M[1,7]+M[4,6]+M[5,5]+M[4,6]))
-	# t5
-	@constraint(m,(c6*v)/24 - (c1*v)/120==-(2*M[1,8]+M[4,7]+M[5,6]+M[5,6]+M[4,7]))
-	# t6
-	@constraint(m,- (c2*v)/720 - (c5*v)/120==-(2*M[1,9]+M[4,8]+M[5,7]+M[6,6]+M[5,7]+M[4,8]))
-	# t7
-	@constraint(m,(c1*v)/5040 - (c6*v)/720==-(2*M[1,10]+M[4,9]+M[5,8]+M[6,7]+M[6,7]+M[5,8]+M[4,9]))
-	# t8
-	@constraint(m,(c5*v)/5040==-(2*M[1,11]+M[4,10]+M[5,9]+M[6,8]+M[7,7]+M[6,8]+M[5,9]+M[4,10]))
-	# t9
-	@constraint(m,-(c1*v)/362880==-(2*M[1,12]+M[4,11]+M[5,10]+M[6,9]+M[7,8]+M[7,8]+M[6,9]+M[5,10]+M[4,11]))
-	# t10
-	@constraint(m,-(c5*v)/362880==-(M[4,12]+M[5,11]+M[6,10]+M[7,9]+M[8,8]+M[7,9]+M[6,10]+M[5,11]+M[4,12]))
-
-
-
-
-	# yt8: 0
-	@constraint(m,0==M[2,11])
-	# xt8: 0
-	@constraint(m,0==M[3,11])
-	# x2: 0
-	@constraint(m,0==M[2,2])
-	# y2: 0
-	@constraint(m,0==M[3,3])
-	# xy: 0
-	@constraint(m,0==2*M[2,3])
-
-
-	#theta11:0
-	@constraint(m,M[5,12]+M[6,11]+M[7,10]+M[8,9]+M[8,9]+M[7,10]+M[6,11]+M[5,12]==0)
-	#theta12:0
-	@constraint(m,M[6,12]+M[7,11]+M[8,10]+M[9,9]+M[8,10]+M[7,11]+M[6,12]==0)
-	#theta13:0
-	@constraint(m,M[7,12]+M[8,11]+M[9,10]+M[9,10]+M[8,11]+M[7,12]==0)
-	#theta14:0
-	@constraint(m,M[8,12]+M[9,11]+M[10,10]+M[9,11]+M[8,12]==0)
-	#theta15:0
-	@constraint(m,M[9,12]+M[10,11]+M[10,11]+M[9,12]==0)
-	#theta16:0
-	@constraint(m,M[10,12]+M[11,11]+M[10,12]==0)
-	#theta17:0
-	@constraint(m,M[11,12]+M[11,12]==0)
-	#theta18:0
-	@constraint(m,M[12,12]==0)
-
-	solve(m)
-
-	C=getvalue([c0 c1 c2 c3 c4 c5 c6 c7 c8 c9])
-    return C
 end
